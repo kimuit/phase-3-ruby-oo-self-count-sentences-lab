88c1550755ab1c38beca6497ae15067971b209e6
kimuit117 <kemoikiuita@gmail.com>
2022-10-27 10:48:34 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "#{self}".end_with?(".")
   end
 
   def question?
-
+     "#{self}".end_with?("?")
   end
 
   def exclamation?
-
+    "#{self}".end_with?("!")
   end
 
-  def count_sentences
-
+   def count_sentences
+  sentence_array = self.scan(/[.!?]+(?=\s|\z)/)
+  return sentence_array.count
   end
+ 
+# THIS ALSO WORKS
+  #  def count_sentences
+  #   scan(/[.!?]+(?=\s|\z)/).size
+  # end
+
+  
 end
